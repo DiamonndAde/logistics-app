dacffb097a2a6bb859db2e1c0aa862ba1489074e
Ayomide Adedeji <diamonditadedeji@gmail.com>
2022-10-10 01:20:28 +0100

    Initial Commit

---
+++ src/NavBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ElectricMopedOutlinedIcon from "@mui/icons-material/ElectricMopedOutlined";
+import { orange } from "@mui/material/colors";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div className="w-full flex p-5">
+      <div className="flex gap-10 items-center w-2/3">
+        <Link className="flex items-center gap-px" to="/">
+          <ElectricMopedOutlinedIcon
+            fontSize="large"
+            sx={{ color: orange[700] }}
+          />
+          <button className="text-xl font-extrabold">Ship 'n Drive</button>
+        </Link>
+        <button className="font-bold">Services</button>
+        <button className="font-bold">Tracking</button>
+        <button className="font-bold">Locations</button>
+      </div>
+      <div className="flex w-1/3 gap-5 justify-center">
+        <button className="text-black font-bold">
+          <Link to="/login">Login</Link>
+        </button>
+        <button className="bg-black hover:bg-orange-500 transition-all ease-out duration-500 text-white rounded-md px-2">
+          <Link to="/">Sign Up</Link>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import NavBar from "./NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ShipperSignup from "./ShipperSignup";
+import RiderSignup from "./RiderSignup";
+import RiderLogin from "./Login";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/shipper-signup" element={<ShipperSignup />} />
+        <Route path="/rider-signup" element={<RiderSignup />} />
+        <Route path="/login" element={<RiderLogin />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
