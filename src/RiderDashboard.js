ad29a478d3af0073e806ce76d0603001f864b815
Omole Daniel <omoledaniel13@gmail.com>
2023-02-07 07:58:13 +0100

    commit

--- src/RiderDashboard.js
+++ src/RiderDashboard.js
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { styles } from './constants/styles';
+import { Human, burger } from "./assets";
+import { Link } from 'react-router-dom';
+import JourneyPlanner from './JourneyPlanner';
+
+// function DbIcon({ src }) {
+//     return <img src={src} className="w-[16px]" alt="Icon" />;
+// }
 
 const RiderDashboard = () => {
     const [activeComponent, setActiveComponent] = useState([]);
     const [isNavOpen, setIsNavOpen] = useState(false);
     const [storeName, setStoreName] = useState("David Long");
-    const [storeLogo, setStoreLogo] = useState("");
+    const [storeLogo, setStoreLogo] = useState(Human);
 
     function showActiveComponent(e, comp) {
         e.stopPropagation();
@@ -40,7 +47,7 @@ const RiderDashboard = () => {
                 >
                     <div className="border-b flex items-center justify-between p-4 mb-[10px]">
                         <h1 className="font-bold text-[16px] ">{storeName}</h1>
-                        {/* <img src={storeLogo} alt="Profile Image" className={storeLogo === "" ? "w-[32px]" : "w-[40px] h-[40px] rounded-full"} /> */}
+                        <img src={storeLogo} alt="Profile Image" className={storeLogo === "" ? "w-[32px]" : "w-[40px] h-[40px] rounded-full"} />
                     </div>
                     <div
                         className={`${styles.dbNavItem}`}
@@ -65,7 +72,7 @@ const RiderDashboard = () => {
                     </div>
                     <div
                         className={`${styles.dbNavItem}`}
-                        // onClick={(e) => showActiveComponent(e, <Finances />)}
+                        onClick={(e) => showActiveComponent(e, <JourneyPlanner />)}
                     >
                         {/* <DbIcon src={finances} /> */}
                         <h2>Journey Planner</h2>
@@ -103,7 +110,22 @@ const RiderDashboard = () => {
                     </div>
                 </div>
                 <div className="lg:w-4/5 w-full shrink-0 bg-brand-gray-200 min-h-screen p-[24px] lg:px-10 lg:py-5 top-0 right-0">
-                    <div>{activeComponent[0]}</div>
+                    <div className="w-full flex justify-between items-center">
+                        <div className="flex gap-8 items-center">
+                            <img
+                                src={burger}
+                                className="w-[24px] block lg:hidden"
+                                alt="Icon"
+                                onClick={handleClick}
+                            />
+                            {/* <Link to={"/store-front"}>
+                                <button className={`${styles.buttonOutline}`}>
+                                Go to store front
+                                </button>
+                            </Link> */}
+                        </div>
+                        <div>{activeComponent[0]}</div>
+                    </div>
                     {/* <p>This is it.</p> */}
                 </div>
             </div>
--- src/assets/index.js
+++ src/assets/index.js
@@ -2,10 +2,14 @@ import user from "./images/users.png";
 import rider from "./images/rider.png";
 import DeliveryGuy from "./images/delivery-guy.png";
 import Flash from "./images/flash.png";
+import Human from "./images/human.png";
+import burger from "./images/burger.png"
 
 export {
     user,
     rider,
     DeliveryGuy,
-    Flash
+    Flash,
+    Human,
+    burger
 }
